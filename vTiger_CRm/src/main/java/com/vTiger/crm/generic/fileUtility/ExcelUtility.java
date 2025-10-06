3322681350deb1f70d51b5371d118f72c37596c8
kalyan0832 <spk9640@gmail.com>
2025-10-06 12:32:01 +0530

    Committing new changes

--- vTiger_CRm/src/main/java/com/vTiger/crm/generic/fileUtility/ExcelUtility.java
+++ vTiger_CRm/src/main/java/com/vTiger/crm/generic/fileUtility/ExcelUtility.java
@@ -6,7 +6,6 @@ import java.io.FileOutputStream;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
-
 /**
  * contains resuable methods of excel file
  * @author Pavan Kalyan
@@ -14,27 +13,27 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class ExcelUtility {
 	public String getDataFromExcel(String sheetName, int rowNum, int cellNum) throws Throwable {
-	FileInputStream fis=new FileInputStream("./TestData/vTigerTestData.xlsx");
-	Workbook wb=WorkbookFactory.create(fis);
+	FileInputStream fis = new FileInputStream("./TestData/vTigerTestData.xlsx");
+	Workbook wb = WorkbookFactory.create(fis);
 	String data = wb.getSheet(sheetName).getRow(rowNum).getCell(cellNum).toString();
 
 	return data;
 	}
-	
-	public int getRowCount(String sheetName)throws Throwable {
-		FileInputStream fis=new FileInputStream("./TestData/vTigerTestData.xlsx");
-		Workbook wb=WorkbookFactory.create(fis);
-		int rowCount=wb.getSheet(sheetName).getLastRowNum();
+
+	public int getRowCount(String sheetName) throws Throwable {
+		FileInputStream fis = new FileInputStream("./TestData/vTigerTestData.xlsx");
+		Workbook wb = WorkbookFactory.create(fis);
+		int rowCount = wb.getSheet(sheetName).getLastRowNum();
 		return rowCount;
 	}
-	
+
 	public void setDataIntoExcel(String sheetName, int rowNum, int cellNum, String data) throws Throwable {
-		FileInputStream fis=new FileInputStream("./TestData/vTigerTestData.xlsx");
-		Workbook wb=WorkbookFactory.create(fis);
+		FileInputStream fis = new FileInputStream("./TestData/vTigerTestData.xlsx");
+		Workbook wb = WorkbookFactory.create(fis);
 		wb.getSheet(sheetName).getRow(rowNum).createCell(cellNum).setCellValue(data);
-		FileOutputStream fout=new FileOutputStream("./TestData/vTigerTestData.xlsx");
+		FileOutputStream fout = new FileOutputStream("./TestData/vTigerTestData.xlsx");
 		wb.write(fout);
 		wb.close();
-		
+
 }
 }
